0da12ca8f74ec3ca3bc3bf33ac92f187d5365ad7
ValeraFurman <48140310+ValeraFurman@users.noreply.github.com>
2022-03-30 00:23:34 +0300

    Add files via upload

---
+++ PID/Form1.Designer.cs
@@ -0,0 +1,341 @@
+﻿
+namespace PID
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.tSetup = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.KpSetup = new System.Windows.Forms.TextBox();
+            this.KiSetup = new System.Windows.Forms.TextBox();
+            this.KdSetup = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.PMW_OUT = new System.Windows.Forms.TextBox();
+            this.tSensor = new System.Windows.Forms.TextBox();
+            this.plotView1 = new OxyPlot.WindowsForms.PlotView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.Dt_TIME = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.loopTIME = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "tSetup";
+            // 
+            // tSetup
+            // 
+            this.tSetup.Location = new System.Drawing.Point(56, 6);
+            this.tSetup.Name = "tSetup";
+            this.tSetup.Size = new System.Drawing.Size(100, 20);
+            this.tSetup.TabIndex = 1;
+            this.tSetup.Text = "37,8";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kp";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Kd";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Ki";
+            // 
+            // KpSetup
+            // 
+            this.KpSetup.Location = new System.Drawing.Point(56, 29);
+            this.KpSetup.Name = "KpSetup";
+            this.KpSetup.Size = new System.Drawing.Size(100, 20);
+            this.KpSetup.TabIndex = 5;
+            this.KpSetup.Text = "900";
+            // 
+            // KiSetup
+            // 
+            this.KiSetup.Location = new System.Drawing.Point(56, 52);
+            this.KiSetup.Name = "KiSetup";
+            this.KiSetup.Size = new System.Drawing.Size(100, 20);
+            this.KiSetup.TabIndex = 6;
+            this.KiSetup.Text = "0,8";
+            // 
+            // KdSetup
+            // 
+            this.KdSetup.Location = new System.Drawing.Point(56, 76);
+            this.KdSetup.Name = "KdSetup";
+            this.KdSetup.Size = new System.Drawing.Size(100, 20);
+            this.KdSetup.TabIndex = 7;
+            this.KdSetup.Text = "10";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(13, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "PMW";
+            // 
+            // PMW_OUT
+            // 
+            this.PMW_OUT.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.PMW_OUT.ForeColor = System.Drawing.Color.Black;
+            this.PMW_OUT.Location = new System.Drawing.Point(56, 106);
+            this.PMW_OUT.Name = "PMW_OUT";
+            this.PMW_OUT.Size = new System.Drawing.Size(100, 20);
+            this.PMW_OUT.TabIndex = 9;
+            // 
+            // tSensor
+            // 
+            this.tSensor.Location = new System.Drawing.Point(162, 6);
+            this.tSensor.Name = "tSensor";
+            this.tSensor.Size = new System.Drawing.Size(100, 20);
+            this.tSensor.TabIndex = 10;
+            this.tSensor.Text = "30,0";
+            // 
+            // plotView1
+            // 
+            this.plotView1.Location = new System.Drawing.Point(2, 132);
+            this.plotView1.Name = "plotView1";
+            this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView1.Size = new System.Drawing.Size(631, 413);
+            this.plotView1.TabIndex = 11;
+            this.plotView1.Text = "plotView1";
+            this.plotView1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(268, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Go to 35C";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.button2.Location = new System.Drawing.Point(162, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "UP";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button3.Location = new System.Drawing.Point(162, 51);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "DW";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(477, 64);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Start";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button5.Location = new System.Drawing.Point(558, 64);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Stop";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Dt_TIME
+            // 
+            this.Dt_TIME.Location = new System.Drawing.Point(533, 12);
+            this.Dt_TIME.Name = "Dt_TIME";
+            this.Dt_TIME.Size = new System.Drawing.Size(100, 20);
+            this.Dt_TIME.TabIndex = 19;
+            this.Dt_TIME.Text = "100";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(511, 15);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(16, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "dt";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(471, 41);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 13);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "void loop()";
+            // 
+            // loopTIME
+            // 
+            this.loopTIME.Location = new System.Drawing.Point(533, 38);
+            this.loopTIME.Name = "loopTIME";
+            this.loopTIME.Size = new System.Drawing.Size(100, 20);
+            this.loopTIME.TabIndex = 21;
+            this.loopTIME.Text = "1000";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label8.Location = new System.Drawing.Point(293, 548);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 15);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "TIME";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 573);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.loopTIME);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.Dt_TIME);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.plotView1);
+            this.Controls.Add(this.tSensor);
+            this.Controls.Add(this.PMW_OUT);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.KdSetup);
+            this.Controls.Add(this.KiSetup);
+            this.Controls.Add(this.KpSetup);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tSetup);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PID TEST";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tSetup;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox KpSetup;
+        private System.Windows.Forms.TextBox KiSetup;
+        private System.Windows.Forms.TextBox KdSetup;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox PMW_OUT;
+        private System.Windows.Forms.TextBox tSensor;
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox Dt_TIME;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox loopTIME;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ PID/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Axes;
+namespace PID
+{
+    public partial class Form1 : Form
+    {
+        float Sensor = 37.3f;
+        float Step = 0.01f;
+        double[] DW_Data = new double[50];
+        float err; float integral = 0, prevErr = 0;
+        float D;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public  float computePID(float input, float setpoint, float kp, float ki, float kd, float dt, int minOut, int maxOut)
+        {
+            err = setpoint - input;
+            integral = Constrain(integral + (float)err * dt * ki, minOut, maxOut);
+            D = (err - prevErr) / dt;
+            prevErr = err;
+            return Constrain(err * kp + integral + D * kd, minOut, maxOut);
+        }
+        public float Constrain(float amt, float low, float high)
+        {
+           return ((amt) < (low) ? (low) : ((amt) > (high) ? (high) : (amt)));
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var model = new PlotModel();
+            var s1 = new TwoColorAreaSeries();
+            s1.Color = OxyColors.Green;
+            s1.Limit = 0;
+
+            Array.Copy(DW_Data, 1, DW_Data, 0, DW_Data.Length - 1);
+            DW_Data[DW_Data.Length - 1] = Convert.ToDouble(PMW_OUT.Text);
+
+            for (int i = 0; i < DW_Data.Length; i++)
+            {
+                s1.Points.Add(new DataPoint(i + 1, DW_Data[i]));
+
+            }
+            model.Series.Add(s1);
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Left });
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom });
+
+            plotView1.Model = model;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Sensor = 37.5f;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Sensor = Sensor + 0.01f;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Sensor = Sensor - 0.01f;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            timer2.Interval = Int32.Parse(Dt_TIME.Text);
+            timer1.Interval = Int32.Parse(loopTIME.Text);
+            timer2.Enabled = true;  timer1.Enabled = true; 
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            timer2.Enabled = false; timer1.Enabled = false;
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            PMW_OUT.Text = Convert.ToString(computePID(Sensor, float.Parse(tSetup.Text),
+float.Parse(KpSetup.Text), float.Parse(KiSetup.Text), float.Parse(KdSetup.Text), Int32.Parse(Dt_TIME.Text), 0, 255));
+            tSensor.Text = Convert.ToString(Sensor);
+        }
+    }
+}
